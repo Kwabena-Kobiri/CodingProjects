82e6431162a486ddcaaa27fcf1323846b0528998
Kwabena-Kobiri <56099212+Kwabena-Kobiri@users.noreply.github.com>
2020-05-07 22:56:53 +0000

    Add files via upload

---
+++ coding.py
@@ -0,0 +1,23 @@
+#INPUT
+"""Our input is the fahrenheit temperature from the woman"""
+fahrenheit_value = float(input("Please Enter the Fahrenheit temperature: \n"))
+#PROCESSING
+"""The conversion from fahrenheit to celsius"""
+celsius_value = (fahrenheit_value - 32) * (5/9) 
+print(round(celsius_value, 2))
+#OUTPUT
+"""Output is the equivalent celsius temperature"""
+
+number = 98.76453
+
+new_number = round(number, 2)
+
+print(new_number)
+
+range()
+
+print('all even numbers from 1 to 10')
+print(range(0, 10, 2))
+
+
+
